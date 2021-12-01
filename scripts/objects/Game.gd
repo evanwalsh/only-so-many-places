345cd65969950037f9041685db68c905bcfeef51
Evan Walsh <evan@hellowelcome.org>
2021-12-01 13:10:51 -0500

    Fix various issues with HTML export

--- scripts/objects/Game.gd
+++ scripts/objects/Game.gd
@@ -5,21 +5,24 @@ const EndGameUI = preload("res://scenes/EndGameUI.tscn")
 
 var mom: Mom
 var player: Player setget _set_player
+var _did_quit := false
 var _retry_scene_path := "res://scenes/Apartment.tscn"
 
 
 func _ready():
+	randomize()
+
 	Events.connect("start_game", self, "_on_start_game")
 	Events.connect("end_game", self, "_on_end_game")
 	Events.connect("quit", self, "_on_quit")
 	Events.connect("retry", self, "_on_retry")
 
 
-func _set_player(value: Player) -> void:
+func _set_player(value) -> void:
 	player = value
 
 	if is_instance_valid(mom):
-		(mom as Node2D).player = value
+		mom.player = value
 
 
 func _on_start_game() -> void:
@@ -51,8 +54,22 @@ func _notification(what: int) -> void:
 			_on_quit()
 
 
+func _input(event: InputEvent) -> void:
+	if (
+		OS.has_feature("web")
+		and !OS.window_fullscreen
+		and event is InputEventMouseButton
+		and !_did_quit
+	):
+		OS.window_fullscreen = true
+
+
 func _on_quit() -> void:
-	get_tree().quit()
+	if OS.has_feature("web"):
+		OS.window_fullscreen = false
+		_did_quit = true
+	else:
+		get_tree().quit()
 
 
 func _on_retry() -> void:
